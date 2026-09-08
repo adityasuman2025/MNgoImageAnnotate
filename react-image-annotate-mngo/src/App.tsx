71d58ba3dfc975d485efd3badd2831b3c4d6eab3
adityasuman2025 <adityasuman2025@gmail.com>
2026-09-08 14:07:49 +0530

    deleted the new folder

--- react-image-annotate-mngo/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-            <h1 className="text-3xl font-bold underline">Tailwind CSS is Ready!</h1>
-        </div>
-    )
-}
-
-export default App
