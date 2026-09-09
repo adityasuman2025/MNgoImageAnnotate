d3136bcb9b24c073019d3003252c8fce0c529ed1
adityasuman2025 <adityasuman2025@gmail.com>
2026-09-09 17:58:51 +0530

    lib setup done

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 // @ts-ignore
 import MNgoImageAnnotate from "../dist/index.es.js";
+import "./index.css";
 
 function App() {
   return (
@@ -10,8 +9,4 @@ function App() {
   )
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<App />);
